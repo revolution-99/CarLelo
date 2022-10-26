742325efdf40c990028be4ee2f8045618ac38ef6
revolution-99 <revolution.ninety9@gmail.com>
2022-10-26 16:29:24 +0530

    Minor Fixes after the final review 1. Remove the dependency destroy error between brand and model 2. Filter car is linked to the show view 3. Minor change in notification view as per car's condition

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,5 +1,5 @@
 class Model < ApplicationRecord
-    belongs_to :brand, dependent: :destroy
+    belongs_to :brand
     
     validates :name, presence: true, uniqueness: { case_sensitive: false }
 end
